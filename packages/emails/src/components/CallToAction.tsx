bc60257af1eab687b25c221d4727d0d313c25c96
Leobert Camoro <leobert.camoro101@gmail.com>
2026-09-12 19:55:09 +0800

    fix: remove @ts-expect-error in CallToAction by splitting anchor and button renders

--- packages/emails/src/components/CallToAction.tsx
+++ packages/emails/src/components/CallToAction.tsx
@@ -24,63 +24,72 @@ export const CallToAction = (props: {
     return `${paddingTop} ${paddingRight} ${paddingBottom} ${paddingLeft}`;
   };
 
-  const El = href ? "a" : "button";
-  const restProps = href ? { href, target: "_blank" } : { type: "submit" };
+  const sharedStyle = {
+    color: secondary ? "#292929" : "#FFFFFF",
+    textDecoration: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "auto",
+    appearance: "none" as const,
+    background: "transparent",
+    border: "none",
+    padding: 0,
+    fontSize: "inherit",
+    fontWeight: 500,
+    lineHeight: "1rem",
+    cursor: "pointer",
+  };
+
+  const content = (
+    <>
+      {startIconName && (
+        <CallToActionIcon
+          style={{ marginRight: "0.5rem", marginLeft: 0 }}
+          iconName={startIconName}
+        />
+      )}
+      {label}
+      {endIconName && <CallToActionIcon iconName={endIconName} />}
+    </>
+  );
+
+  const containerStyle = {
+    display: "inline-block",
+    background: secondary ? "#FFFFFF" : "#292929",
+    border: secondary ? "1px solid #d1d5db" : "",
+    color: "#ffffff",
+    fontFamily: "Roboto, Helvetica, sans-serif",
+    fontSize: "0.875rem",
+    fontWeight: 500,
+    lineHeight: "1rem",
+    margin: 0,
+    textDecoration: "none",
+    textTransform: "none" as const,
+    padding: calculatePadding(),
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    msoPaddingAlt: "0px",
+    borderRadius: "6px",
+    boxSizing: "border-box" as const,
+    height: "2.25rem",
+  };
+
+  if (href) {
+    return (
+      <p style={containerStyle}>
+        <a href={href} target="_blank" rel="noreferrer" style={sharedStyle}>
+          {content}
+        </a>
+      </p>
+    );
+  }
 
   return (
-    <p
-      style={{
-        display: "inline-block",
-        background: secondary ? "#FFFFFF" : "#292929",
-        border: secondary ? "1px solid #d1d5db" : "",
-        color: "#ffffff",
-        fontFamily: "Roboto, Helvetica, sans-serif",
-        fontSize: "0.875rem",
-        fontWeight: 500,
-        lineHeight: "1rem",
-        margin: 0,
-        textDecoration: "none",
-        textTransform: "none",
-        padding: calculatePadding(),
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        msoPaddingAlt: "0px",
-        borderRadius: "6px",
-        boxSizing: "border-box",
-        height: "2.25rem",
-      }}>
-      {/* @ts-expect-error shared props between href and button */}
-      <El
-        style={{
-          color: secondary ? "#292929" : "#FFFFFF",
-          textDecoration: "none",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "auto",
-          appearance: "none",
-          background: "transparent",
-          border: "none",
-          padding: 0,
-          fontSize: "inherit",
-          fontWeight: 500,
-          lineHeight: "1rem",
-          cursor: "pointer",
-        }}
-        {...restProps}
-        rel="noreferrer">
-        {startIconName && (
-          <CallToActionIcon
-            style={{
-              marginRight: "0.5rem",
-              marginLeft: 0,
-            }}
-            iconName={startIconName}
-          />
-        )}
-        {label}
-        {endIconName && <CallToActionIcon iconName={endIconName} />}
-      </El>
+    <p style={containerStyle}>
+      <button type="submit" rel="noreferrer" style={sharedStyle}>
+        {content}
+      </button>
     </p>
   );
 };
