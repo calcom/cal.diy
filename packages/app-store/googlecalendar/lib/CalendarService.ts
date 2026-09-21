38dd1ae45ebbe7f7dd4dd7189a2f2d26263fbbf2
quwrof00 <shouryaagrawal2806@gmail.com>
2026-09-22 01:55:33 +0530

    fix(googlecalendar): prevent duplicate updated invitation emails on event creation

--- packages/app-store/googlecalendar/lib/CalendarService.ts
+++ packages/app-store/googlecalendar/lib/CalendarService.ts
@@ -284,7 +284,7 @@ class GoogleCalendarService implements Calendar {
           calendarId: selectedCalendar,
           requestBody: payload,
           conferenceDataVersion: 1,
-          sendUpdates: "none",
+          sendUpdates: "all",
         });
         event = eventResponse.data;
         if (event.recurrence) {
@@ -300,6 +300,7 @@ class GoogleCalendarService implements Calendar {
           // Update the same event but this time we know the hangout link
           calendarId: selectedCalendar,
           eventId: event.id || "",
+          sendUpdates: "none",
           requestBody: {
             description: getRichDescription({
               ...calEvent,
